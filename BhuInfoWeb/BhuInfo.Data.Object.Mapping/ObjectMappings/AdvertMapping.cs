133ed431fb5dd156af08c6c8053583484d417d3b
BABAYEGAR <salxsaa@gmail.com>
2016-09-21 23:26:54 +0100

    Added the Advertisement Data Object, Mapping and context classes..

---
+++ BhuInfoWeb/BhuInfo.Data.Object.Mapping/ObjectMappings/AdvertMapping.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using BhuInfo.Data.Objects.Entities;
+
+namespace BhuInfo.Data.Object.Mapping.ObjectMappings
+{
+    public class AdvertMapping : EntityTypeConfiguration<Advertisement>
+    {
+        public AdvertMapping()
+        {
+            //this property is a primary key
+            HasKey(n => n.AdvertId);
+
+            //this are the remaining properties
+            Property(n => n.AdvertCompanyName)
+                .IsRequired();
+            Property(n => n.AdvertLogo)
+                .IsRequired();
+            Property(n => n.AdvertText)
+                .IsRequired();
+            //mapping the object entities to their respective columns
+            ToTable("Advertisement");
+            Property(n => n.AdvertId).HasColumnName("AdvertId");
+            Property(n => n.AdvertLogo).HasColumnName("AdvertLogo");
+            Property(n => n.AdvertCompanyName).HasColumnName("AdvertCompanyName");
+            Property(n => n.AdvertText).HasColumnName("AdvertText");
+        }
+    }
+}
---
+++ BhuInfoWeb/BhuInfo.Data.Objects/Entities/Advertisement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BhuInfo.Data.Objects.Entities
+{
+    public class Advertisement
+    {
+        public long AdvertId { get; set; }
+        public string AdvertLogo { get; set; }
+        public string AdvertText { get; set; }
+        public string AdvertCompanyName { get; set; }
+    }
+}
